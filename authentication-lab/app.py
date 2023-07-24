1f217f88a551fbbf8c92fcb261c2210ad69161c4
GuyWi24-meet <1963jackwilliams@gmail.com>
2023-07-24 14:11:16 +0300

    yessir

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -14,7 +14,7 @@ config = {
   "messagingSenderId": "286426368528",
   "appId": "1:286426368528:web:6a67e87b9cd50f3eaf5895",
   "measurementId": "G-SWDS6VP6GH",
-  "databaseURL": ""
+  "databaseURL": "https://rogalachem-default-rtdb.europe-west1.firebasedatabase.app/"
 }
 
 @app.route('/', methods=['GET', 'POST'])
@@ -28,8 +28,11 @@ def signin():
             return redirect(url_for('home'))
         except:
             error = "Authentication failed"
-            return render_template("signin.html")
+    return render_template("signin.html")
 
+
+@app.route('/signup', methods=['GET', 'POST'])
+# ... (existing code)
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
@@ -37,22 +40,65 @@ def signup():
     if request.method == 'POST':
         email = request.form['email']
         password = request.form['password']
-        try:
-            login_session['user'] = auth.create_user_with_email_and_password(email, password)
-            return redirect(url_for('home'))
-        except:
-            error = "Authentication failed"
-            return render_template("signup.html")
+        username = request.form['username']
+        Bio = request.form['Bio']
 
+        try:
+            user = auth.create_user_with_email_and_password(email, password)
+
+            user_data = {
+                'email': email,
+                'password': password,
+                'username': username,
+                'Bio': Bio
+            }
+
+            db.child("Users").child(user['localId']).set(user_data)
+
+            login_session['user'] = user
+            return redirect(url_for('home'))
+        except Exception as e:
+            error = "Authentication failed: " + str(e)
+
+    return render_template("signup.html", error=error)
 
 @app.route('/add_tweet', methods=['GET', 'POST'])
 def add_tweet():
+    if request.method == 'POST':
+        title = request.form['title']
+        text = request.form['text']
+
+        if 'user' in login_session:
+            user_uid = login_session['user']['localId']
+
+            tweet = {
+                'title': title,
+                'text': text,
+                'uid': user_uid
+            }
+
+            try:
+                db.child("Tweets").push(tweet)
+                return redirect(url_for('home'))
+            except Exception as e:
+                print("Error adding tweet to the database:", str(e))
+        
     return render_template("add_tweet.html")
+
+@app.route('/all_tweets')
+def all_tweets():
+    try:
+        tweets = db.child("Tweets").get().val()
+        return render_template("tweets.html", tweets=tweets)
+    except Exception as e:
+        print("Error retrieving tweets from the database:", str(e))
+
+    return "Error: Unable to retrieve tweets."
 
 
 firebase = pyrebase.initialize_app(config)
 auth = firebase.auth()
-
+db = firebase.database()
 
 if __name__ == '__main__':
     app.run(debug=True)
